41932225d90854125048de4f0ebec94ba8bd4249
Gowiem <MGowie@apprenaissance.com>
2013-10-13 20:42:40 -0400

    Creates base Rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sisyphus::Application.config.secret_key_base = '7ea04abc3b66245140b43dbf95103cd633c5707b943db1c0ac42284a95bbda40acaa34d40adfb490446a7df7496613bc31be20e02cdb41b686dd032211508da5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sisyphus::Application.config.session_store :cookie_store, key: '_Sisyphus_session'
