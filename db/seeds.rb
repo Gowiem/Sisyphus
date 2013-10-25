d7f79c1a3e79c046643b08aa1d3c350fa346d817
Gowiem <MGowie@apprenaissance.com>
2013-10-24 23:54:01 -0400

    Adds seed data for Required and Sub tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,11 @@
 
 puts "--- CREATING SEED DATA ---"
 
+## Users
+#########
+
+
+## Teacher
 teach = Teacher.create!(
   email: "mark@email.com",
   first_name: "Mark",
@@ -15,6 +20,7 @@ teach = Teacher.create!(
   password: "password12", 
   password_confirmation: "password12")
 
+## Students
 gowie = Student.create!(
   email: "gowie@email.com",
   first_name: "Matt",
@@ -50,9 +56,12 @@ sarah = Student.create!(
   password: "password12", 
   password_confirmation: "password12")
 
+## Project Groups
+##################
 devs = ProjectGroup.create!( name: "Developers", students: [ gowie, craig ])
 design = ProjectGroup.create!( name: "Designers", students: [ maple, john, sarah ])
 
+## Project
 sisyphus = Project.create!(
   title: "Sisyphus Project", 
   students: [ gowie, craig, maple, john, sarah ],
@@ -60,7 +69,54 @@ sisyphus = Project.create!(
   project_groups: [ design, devs ]
 )
 
+## Tasks
+#########
+
+## Required Tasks
+dev_req_task1 = RequiredTask.create!(
+  title: "Learn Ember.js", 
+  description: "No cake walk, but mad cool.", 
+  project: sisyphus
+)
+
+dev_req_task2 = RequiredTask.create!(
+  title: "Write some Unit Tests", 
+  description: "If theres time to breath... :pray:", 
+  project: sisyphus
+)
+
+design_req_task1 = RequiredTask.create!(
+  title: "Design all the things!",
+  description: "Pretty important, no pressure.",
+  project: sisyphus
+)
+
+## Subtasks
+dev_task1 = Subtask.create!(
+  title: "Run through the Ember tutorial",
+  description: "It's aight..",
+  project_group: devs,
+  parent_task: dev_req_task1
+)
+
+dev_task2 = Subtask.create!(
+  title: "Write Unit Tests for the frontend",
+  description: "Not sure how we'll go about this yet",
+  project_group: devs,
+  parent_task: dev_req_task2
+)
+
+design_task1 = Subtask.create!(
+  title: "Design the tasks screen",
+  description: "This is pretty meta right here.",
+  project_group: design,
+  parent_task: design_req_task1
+)
+
 puts "--- Created Project: #{sisyphus.title}"
-puts "--- Project has teacher: #{sisyphus.teacher.full_name} \n"
-puts "--- Project has associated students: #{sisyphus.students.entries.map(&:full_name)} \n"
+puts "--- Project has Teacher: #{sisyphus.teacher.full_name} \n"
+puts "--- Project has Students: #{sisyphus.students.entries.map(&:full_name)} \n"
 puts "--- Project has Project Groups: #{sisyphus.project_groups.entries.map(&:name)}"
+puts "--- Project has Required Tasks: #{sisyphus.required_tasks.map(&:title)}"
+puts "--- Dev Subtasks: #{devs.subtasks.map(&:title)}"
+puts "--- Design Subtasks: #{design.subtasks.map(&:title)}"
