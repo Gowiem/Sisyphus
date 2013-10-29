afb5ce3b23b31eaf2d15ad7b49337a66609ff2bc
Gowiem <MGowie@apprenaissance.com>
2013-10-29 14:12:17 -0400

    Adds is_completed and due_date to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < BaseDocument
   field :title
-  field :type, :type => String
+  field :is_completed, type: Boolean, default: false
+  field :due_date, type: Date
+
+  field :type, type: String
 end
