bf12458b916678c4957b3fb1a0343ca75187cca7
Gowiem <MGowie@apprenaissance.com>
2013-10-29 14:01:58 -0400

    Adds back required_task_view.js as it broke adding subtasks

---
+++ app/assets/javascripts/views/required_task_view.js
@@ -0,0 +1,4 @@
+Sis.RequiredTaskView = Ember.View.extend({
+  // This view needs to be here for the new task view to get it's parent
+  // required task and assign it to the parentTask relationship. 
+});
