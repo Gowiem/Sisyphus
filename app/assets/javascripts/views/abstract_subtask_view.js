6724cf9b11a7ebf29ea0fbd0b970820435888f8c
Gowiem <MGowie@apprenaissance.com>
2013-10-31 22:17:33 -0400

    Fixes those damn user checkboxes.

--- app/assets/javascripts/views/abstract_subtask_view.js
+++ app/assets/javascripts/views/abstract_subtask_view.js
@@ -3,15 +3,4 @@ Sis.AbstractSubtaskView = Ember.View.extend({
     // initialize the jQuery datepicker we're using once it's in the dom
     $('.datepicker').datepicker();
   },
-  // Overrides Ember.Checkbox to add the checked student to this content
-  userCheckbox: Em.Checkbox.extend({
-    checkedObserver: function(){
-      var content = this.get('controller').get('content')
-      if(this.get('checked')) {
-        content.get('students').pushObject(this.get('student'));
-      } else {
-        content.get('students').removeObject(this.get('student'));
-      }
-    }.observes('checked')
-  }),
 });
