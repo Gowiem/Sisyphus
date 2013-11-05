7fbea854f372a5b8223f916c982998dc3b42d8d1
Gowiem <gowie.matt@gmail.com>
2013-11-05 16:32:45 -0500

    Adds dates to comment.hanlebars

--- app/assets/javascripts/controllers/comment_controller.js
+++ app/assets/javascripts/controllers/comment_controller.js
@@ -2,6 +2,10 @@ Sis.CommentController = Ember.ObjectController.extend({
   canDelete: function() {
     return this.get('model.user.id') === Sis.currentUser.id;
   }.property('model.user'),
+  readableCreateAtDate: function() {
+    var createdAt = this.get('model.createdAt');
+    return moment(createdAt).format('DD/MM hh:mm A');
+  }.property('model.createdAt'),
   actions: {
     deleteComment: function() {
       var comment = this.get('model');
--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -2,4 +2,6 @@ Sis.Comment = DS.Model.extend({
   body: DS.attr('string'),
   subtask: DS.belongsTo('subtask'),
   user: DS.belongsTo('student'),
+  createdAt: DS.attr('isodate'),
+  updatedAt: DS.attr('isodate')
 });
