dd4a07fbf06fefe16c7c52bc0e359483873ef3de
Gowiem <gowie.matt@gmail.com>
2013-11-14 20:17:13 -0500

    Hide sign up link when on the sign up screen

--- app/assets/javascripts/controllers/navigation_controller.js
+++ app/assets/javascripts/controllers/navigation_controller.js
@@ -1,6 +1,9 @@
 Sis.NavigationController = Ember.ObjectController.extend({
-  needs: "auth",
+  needs: ['auth', 'application'],
   isAuthenticated: Em.computed.alias("controllers.auth.isAuthenticated"),
+  onSignupRoute: function() {
+    return this.get('controllers.application.currentPath') === 'registration';
+  }.property('controllers.application.currentPath'),
   actions: {
     logout: function() {
       this.get('controllers.auth').logout();
