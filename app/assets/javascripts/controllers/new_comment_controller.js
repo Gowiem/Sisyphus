2735915c9babea5bb1517068b786fcc90a5512e5
Gowiem <gowie.matt@gmail.com>
2013-12-03 18:24:17 -0500

    Fixes comments saving w/o a user. Fixes #9

--- app/assets/javascripts/controllers/new_comment_controller.js
+++ app/assets/javascripts/controllers/new_comment_controller.js
@@ -6,7 +6,7 @@ Sis.NewCommentController = Ember.ObjectController.extend({
           subtask = this.get('target.target.model'),
           currentUser = this.get('auth.currentUser');
 
-      content.set('user', currentUser.get('content'));
+      content.set('user', currentUser);
       subtask.get('comments').addObject(content);
       content.set('subtask', subtask);
 
