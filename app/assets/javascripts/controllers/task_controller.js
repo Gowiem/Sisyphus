18e828f6604deb9ec449f2e8c706840073ea74ac
Gowiem <gowie.matt@gmail.com>
2013-12-06 15:38:37 -0500

    Red bg for tasks due within 2 days. Fixes #4

--- app/assets/javascripts/controllers/task_controller.js
+++ app/assets/javascripts/controllers/task_controller.js
@@ -3,4 +3,10 @@ Sis.TaskController = Ember.ObjectController.extend({
     var dueDate = this.get('model.dueDate');
     return dueDate ? moment(dueDate).format('D/M') : null;
   }.property('model.dueDate'),
+
+  isDueSoonish: function() {
+    var dueDate = moment(this.get('model.dueDate')),
+        twoDaysFromNow = moment().add('days', 2);
+    return dueDate.isBefore(twoDaysFromNow);
+  }.property('model.dueDate'),
 });
