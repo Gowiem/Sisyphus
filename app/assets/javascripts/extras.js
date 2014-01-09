31204e7f67f2a36b4628ed7f324d69177de4db96
Gowiem <gowie.matt@gmail.com>
2014-01-09 17:20:55 -0500

    Sets CSRF token via cookie. Fixes #11
    Overrides #verified_request? following: http://stackoverflow.com/a/15761835/1159410
    Adds $.cookie

--- app/assets/javascripts/extras.js
+++ app/assets/javascripts/extras.js
@@ -7,3 +7,12 @@ Sis.urls.studentLogout = "/students/sign_out.json";
 Sis.urls.teacherLogin = "/teachers/sign_in.json";
 Sis.urls.teacherRegister = "/teachers.json";
 Sis.urls.teacherLogout = "/teachers/sign_out.json";
+
+// Make sure new CSRF tokens which are passed over to the client via 
+// ApplicationController#set_csrf_token are added to all ajax requests.
+// New tokens come over when we login/logout
+// Pretty much followed this SO post: http://stackoverflow.com/a/15761835/1159410
+$.ajaxPrefilter(function(options, originalOptions, xhr) {
+  var token = $.cookie('XSRF-TOKEN');
+  xhr.setRequestHeader('X-CSRF-Token', token);
+});
