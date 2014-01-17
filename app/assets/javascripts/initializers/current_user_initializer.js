9ddeaa9f2c9b74d54e79472850f32ce52e75b064
Gowiem <gowie.matt@gmail.com>
2014-01-17 17:00:00 -0500

    Fixes the user serialization bug correctly
    Switching our user_serializer to use camelCased keys was a mistake. It was
    causing the typical serialization of our user models to break. What we needed to 
    do (and what this commit does) is to normalize our user JSON before pushing it 
    into our store. Checkout extras.js => Sis#normalizeJsonObject for more info.

--- app/assets/javascripts/initializers/current_user_initializer.js
+++ app/assets/javascripts/initializers/current_user_initializer.js
@@ -3,17 +3,24 @@ Sis.initializer({
 
   initialize: function(container, application) {
     var store = container.lookup('store:main'),
-        jsonString = $('meta[name="current-user"]').attr('content'),
-        user = null;
+        jsonString = $('meta[name="current-user"]').attr('content');
+
+    // Set our currentUser (currently null, but is set again later if found)
+    // into our auth_controller and currentUser controller.
+    container.lookup('controller:auth').set('currentUser', null);
+    container.lookup('controller:currentUser').set('content', null);
+    // Inject our auth_controller into all controllers and routes
+    application.inject('controller', 'auth', 'controller:auth');
+    application.inject('route', 'auth', 'controller:auth');
 
     // If the JSON got set into the current-user meta tag then add it the app
     if (jsonString) {
       var userJson = JSON.parse(jsonString),
-          userType = userJson['type'].toLowerCase(),
-          controller;
-          
+          userType = userJson['type'].toLowerCase();
+
+      userJson = Sis.normalizeJsonObject(userJson, userType, store);
+
       // Push the user model into the store and then grab the newly created user model
-      // TODO: Expecting students here, so we'll have to fix this later to work with teachers as well.
       store.push(userType, userJson);
       store.find(userType, userJson.id).then(function(user) {
         container.lookup('controller:auth').set('currentUser', user);
@@ -23,13 +30,5 @@ Sis.initializer({
       // Remove the current-user meta tag so we don't have user data just lying around
       $('meta[name="current-user"]').remove();
     }
-
-    container.lookup('controller:auth').set('currentUser', user);
-    container.lookup('controller:currentUser').set('content', user);
-    application.inject('controller', 'auth', 'controller:auth');
-    application.inject('route', 'auth', 'controller:auth');
-
-    // Set the auth controllers's content to this user (or null is none was passed in the meta tag) 
-    // and inject the controller into all the things.
   }
 });
