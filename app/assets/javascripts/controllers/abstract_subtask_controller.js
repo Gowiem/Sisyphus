83ad192eba47950a6ef1651ae6bd00584385f81f
Gowiem <gowie.matt@gmail.com>
2014-01-18 19:14:11 -0500

    Remove 'select' button from edit subtaskPartial
    Fixes #16

--- app/assets/javascripts/controllers/abstract_subtask_controller.js
+++ app/assets/javascripts/controllers/abstract_subtask_controller.js
@@ -18,23 +18,4 @@ Sis.AbstractSubtaskController = Ember.ObjectController.extend({
     var hasTitle = this.get('content.title') !== undefined && this.get('content.title') !== '';
     return hasAssignedStudents && hasTitle;
   }.property('content.students.length', 'content.title'),
-  actions: {
-    toggleUserSelect: function() {
-      var controller = this;
-      controller.toggleProperty('isShowingUsers');
-      if (controller.get('isShowingUsers')) {
-        Ember.run.next(this, function() {
-          // Setup the click handler to set isShowingUsers to false
-          $('body').one('click', function() {
-            controller.toggleProperty('isShowingUsers');
-          });
-          // Make sure the select-users-container doesn't trigger the above event
-          $(".select-users-container").click(function(e) {
-            e.stopPropagation();
-            return true;
-          });
-        });
-      }
-    },
-  }
 });
