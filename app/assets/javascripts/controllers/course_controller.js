dd3ff3bbfbce15b0275d4bb913b97ee03b9ee55c
Craig Berry <rjbcc58@gmail.com>
2014-01-23 15:14:29 -0500

    Correct course nav items with filter on course controller.

--- app/assets/javascripts/controllers/course_controller.js
+++ app/assets/javascripts/controllers/course_controller.js
@@ -1,13 +1,10 @@
 Sis.CourseController = Ember.ObjectController.extend({
   selectedProject: null,
 
-  sameAsSelectedCourse: function () {
-    debugger
-  }.property('auth.currentUser.selectedCourse'),
-
   nonActiveCourses: function () {
+    var id = this.get('id');
     return this.get('auth.currentUser.courses').filter(function (course, idx) {
-      return !(this.get('id')===idx);
-    });  
-  }.property('auth.currentUser.courses'),
+      return !(id===course.get('id'));
+    });
+  }.property('auth.currentUser.courses.length'),
 });
