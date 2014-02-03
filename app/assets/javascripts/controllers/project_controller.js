849bdc1ca4ff30ed53578d87e57d52b7a7da67f0
Gowiem <gowie.matt@gmail.com>
2014-02-02 21:13:59 -0500

    Remove unused projectController#progressBarSize

--- app/assets/javascripts/controllers/project_controller.js
+++ app/assets/javascripts/controllers/project_controller.js
@@ -21,27 +21,6 @@ Sis.ProjectController = Ember.ObjectController.extend({
     }
   }.property('requiredTasks', 'showingAllTasks'),
 
-  progressBarSize: function() {
-    var totalTasks = 0,
-        completedTasks = 0, 
-        percentCompleted;
-    // Loop through the requiredTasks to find the count of the overall and 
-    // completed subtasks
-    this.get('requiredTasks').forEach(function(item, idx){
-      var overallCount = item.get('subtasks.length');
-      var completedCount = item.get('subtasks').filterBy('isCompleted').get('length');
-      totalTasks += overallCount;
-      completedTasks += completedCount;
-    });
-    percentCompleted = (completedTasks / totalTasks) * 100;
-    return "width: " + percentCompleted + "%;";
-
-    // Counldn't figure out what to key this computer propety off of since you 
-    // can't chain @each calls like so: 'requiredTasks.@each.subtasks.@each.isCompleted'
-    // so for now we'll just fire the propety change event manually when subtask 
-    // is completed/uncompleted. 
-  }.property(),
-
   // Actions
   ///////////
   actions: {
