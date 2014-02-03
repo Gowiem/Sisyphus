1ef0cb90991e615f63a08d86d516ecddac2ab45d
Gowiem <gowie.matt@gmail.com>
2014-02-02 21:16:05 -0500

    Adds empty news feed message

--- app/assets/javascripts/controllers/news_feed_controller.js
+++ app/assets/javascripts/controllers/news_feed_controller.js
@@ -1,4 +1,8 @@
 Sis.NewsFeedController = Ember.ArrayController.extend({
   sortProperties: ["createdAt"],
   sortAscending: false,
+
+  noHistoryItems: function() {
+    return this.get('content.length') <= 0;
+  }.property('content.length')
 });
