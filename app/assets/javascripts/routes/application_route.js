5bae1c2f1599c39763eda4daf97e2409c88d09a0
Craig Berry <rjbcc58@gmail.com>
2014-02-18 12:41:40 -0500

    Alters application route, fixes login issues

--- app/assets/javascripts/routes/application_route.js
+++ app/assets/javascripts/routes/application_route.js
@@ -3,9 +3,8 @@ Sis.ApplicationRoute = Ember.Route.extend({
     console.log("Model: app route");
     currentUser = this.get('auth.currentUser');
     if (currentUser != null) {
-      if (currentUser.get('isAuthenticated') && currentUser.get('isTeacher')) {
-        return this.get('store').find('semester');
-      }
+      console.log("Model: Returns find semesters");
+      return this.get('store').find('semester');
     }
 
   },
