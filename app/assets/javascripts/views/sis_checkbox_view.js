51c96a57e199e5ec81a74cc2d91d6d606567988b
Gowiem <gowie.matt@gmail.com>
2014-02-22 15:24:44 -0500

    Fixes task opening/closing on checkbox click

--- app/assets/javascripts/views/sis_checkbox_view.js
+++ app/assets/javascripts/views/sis_checkbox_view.js
@@ -3,5 +3,10 @@ Sis.CheckboxView = Ember.Checkbox.extend({
   dataToggle: 'checkbox',
   didInsertElement: function() {
     this.$().checkbox();
+    // Stop the event from bubbling up to subtask so the open/close action
+    // isn't triggered
+    this.$().parent('.checkbox-label').on('click', function(event) {
+      event.stopPropagation();
+    });
   }
 });
