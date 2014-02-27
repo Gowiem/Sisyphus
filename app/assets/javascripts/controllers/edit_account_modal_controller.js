83f3966c0862602370d366dbfbd21f7b95a9ad60
Gowiem <gowie.matt@gmail.com>
2014-02-27 14:23:35 -0500

    Sets firstName & lastName to only update on save
    Fixes #72

--- app/assets/javascripts/controllers/edit_account_modal_controller.js
+++ app/assets/javascripts/controllers/edit_account_modal_controller.js
@@ -3,6 +3,26 @@ Sis.EditAccountModalController = Ember.ObjectController.extend({
   // made any changes. Our original content is set in the editAccount action 
   // in the navigation controller.
   contentCopy: null,
+  firstNameVal: null,
+  lastNameVal: null,
+
+  firstName: function(key, value) {
+    if (value) {
+      this.set('firstNameVal', value);
+      return value;
+    } else {
+      return this.get('model.firstName');
+    }
+  }.property('model.firstName'),
+
+  lastName: function(key, value) {
+    if (value) {
+      this.set('lastNameVal', value);
+      return value;
+    } else {
+      return this.get('model.lastName');
+    }
+  }.property('model.lastName'),
 
   resetPasswordFields: function() {
     this.set('password', null);
@@ -14,27 +34,25 @@ Sis.EditAccountModalController = Ember.ObjectController.extend({
     // TODO: Should do some validation on the email, password before allowing
     // a save.
 		saveUser: function () {
-			var currentContent = this.get('content'),
-          contentCopy    = this.get('contentCopy')
-          currentJsonString = JSON.stringify(currentContent.toJSON()),
-          copyJsonString    = JSON.stringify(contentCopy),
-          self = this;
+			var content = this.get('content'),
+          firstName = this.get('firstNameVal') || this.get('content.firstName'),
+          lastName  = this.get('lastNameVal') || this.get('content.lastName');
+
+      this.set('content.firstName', firstName);
+      this.set('content.lastName', lastName);
+
       this.set('status', null);
-      if (currentJsonString !== copyJsonString) {
-        currentContent.save()
+      content.save()
         // Success Callback
         .then(function(response) {
-          self.set('status', "Successfully updated your account info.");
-          self.set('contentCopy', currentContent.toJSON());
-          self.resetPasswordFields();
+          this.set('status', "Successfully updated your account info.");
+          this.resetPasswordFields();
+        }.bind(this),
         // Error Callback
-        }, function(response) {
+        function(response) {
           console.log("ERROR - response: ", response);
-          self.set('status', "Sorry, there was an error updating your account info. Please try again later.");
-        });
-      } else {
-        console.log("Content wasn't changed, so not updating");
-      }
+          this.set('status', "Sorry, there was an error updating your account info. Please try again later.");
+        }.bind(this));
 		},
 	}
 });
