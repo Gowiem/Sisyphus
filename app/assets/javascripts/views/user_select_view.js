4f693d9504fdaa55c649d125547900afd4872c3e
Gowiem <gowie.matt@gmail.com>
2014-03-21 13:41:38 -0400

    Updates userSelect to allow selection on new subtask

--- app/assets/javascripts/views/user_select_view.js
+++ app/assets/javascripts/views/user_select_view.js
@@ -2,7 +2,8 @@ Sis.UserSelectView = Ember.View.extend({
   classNames: ['tag'],
   classNameBindings: ['context.userSelected:active:inactive', 'context.progressBarClass'],
   click: function() {
-    if (this.get('context.target.constructor') == 'Sis.EditSubtaskController') {
+    if (this.get('context.target.constructor') == 'Sis.EditSubtaskController' ||
+        this.get('context.target.constructor') == 'Sis.NewSubtaskController') {
       this.toggleProperty('context.userSelected');
     }
   },
