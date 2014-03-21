a8f30a96a476e6ee11167c8c2f9fa7550570d2af
Gowiem <gowie.matt@gmail.com>
2014-03-21 12:37:40 -0400

    Updates subtasks to show assigned students as tags
    Removes 'Assigned to:' text + user names and adds the tags that we use on editing to the subtask show view. Does a bunch of cleaning up of the tags css as it was kinda all over the place (Sorry I guess?). Updates user_select_view.js to make sure clicking on the tags only adds/removes them when the user is editing. Fixes #59.

--- app/assets/javascripts/views/user_select_view.js
+++ app/assets/javascripts/views/user_select_view.js
@@ -2,6 +2,8 @@ Sis.UserSelectView = Ember.View.extend({
   classNames: ['tag'],
   classNameBindings: ['context.userSelected:active:inactive', 'context.progressBarClass'],
   click: function() {
-    this.toggleProperty('context.userSelected');
+    if (this.get('context.target.constructor') == 'Sis.EditSubtaskController') {
+      this.toggleProperty('context.userSelected');
+    }
   },
 });
