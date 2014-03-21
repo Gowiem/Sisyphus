cd5b634e8765ed79493dc76ec68a51a3e7519398
Gowiem <gowie.matt@gmail.com>
2014-03-20 20:07:36 -0400

    Adds animation to group-member-info hide/show

---
+++ app/assets/javascripts/views/group_member_view.js
@@ -0,0 +1,8 @@
+Sis.GroupMemberView = Ember.View.extend({
+  didInsertElement: function() {
+    this.addObserver('controller.isShowingGroupMemberInfo', this, this.animateGroupMemberInfo);
+  },
+  animateGroupMemberInfo: function() {
+    this.$('.group-member-info-container').slideToggle('slow');
+  }
+});
