ac474653a7b9ddea6c302a7f6e7bdf67c86e5c6a
Craig Berry <rjbcc58@gmail.com>
2014-03-25 12:17:30 -0400

    Remove unused teacher and course route

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,8 +1,6 @@
 // For more information see: http://emberjs.com/guides/routing/
 Sis.Router.map(function() {
   this.route('home', { path: '/' });
-  this.route('teacher', { path: '/teacher' });
-  this.route('course', { path: '/course/:course_id'});
   this.route('semester', { path: '/semester/:semester_id'});
   this.route('project', { path: '/projects/:project_id'});
   
