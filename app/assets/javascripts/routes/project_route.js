9b4e406d25b4cb6441c7f65b96f1c7aeb9a589f6
Craig Berry <rjbcc58@gmail.com>
2014-03-25 12:33:34 -0400

    removes typo from project_route

--- app/assets/javascripts/routes/project_route.js
+++ app/assets/javascripts/routes/project_route.js
@@ -12,7 +12,7 @@ Sis.ProjectRoute = Ember.Route.extend({
   setupController: function(controller, project) {
     // Setup our model and projectGroup on the projectController as we only have one. 
     controller.set('model', project);
-    controller.set('projectGroup', project.get('projectGroups').objectAt(0));W
+    controller.set('projectGroup', project.get('projectGroups').objectAt(0));
 
     // Setup the requiredTasks controller
     this.controllerFor('requiredTasks').set('content', project.get('requiredTasks'));
